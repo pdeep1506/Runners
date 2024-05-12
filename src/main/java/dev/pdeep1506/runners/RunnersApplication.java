cd4445e31b1ceab9fd7c3ea8e8c61331903a98b0
pdeep1506 <pdeep1506@gmail.com>
2024-05-12 13:04:05 -0400

    init project

---
+++ src/main/java/dev/pdeep1506/runners/RunnersApplication.java
@@ -0,0 +1,21 @@
+package dev.pdeep1506.runners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersApplication {
+
+
+//	private static final Logger log = LoggerFactory.getLogger(RunnersApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(RunnersApplication.class, args);
+
+//		log.info("Main Application started successfully.");
+
+
+	}
+
+
+
+}
