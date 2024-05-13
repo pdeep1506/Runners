ec5458d2ff13211378b67b74e11a563a01c2f513
pdeep1506 <pdeep1506@gmail.com>
2024-05-13 10:38:57 -0400

    completed crud operations

--- src/main/java/dev/pdeep1506/runners/run/RunController.java
+++ src/main/java/dev/pdeep1506/runners/run/RunController.java
@@ -1,20 +1,66 @@
 package dev.pdeep1506.runners.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+
+@RequestMapping("/api/runs")
 public class RunController {
 
 
+  private final RunRepository runRepository;
 
-    @GetMapping("/hello")
-    String home(){
 
-        return "Hello, Runnerz!";
+  public RunController(RunRepository runRepository){
+      this.runRepository = runRepository;
+  }
+
+  @GetMapping("")
+   List<Run> findAll(){
+       return runRepository.findAll();
+   }
+
+
+   @GetMapping("/{id}")
+   Run findById(@PathVariable Integer id) {
+      Optional<Run> run = runRepository.FindById(id);
+      if(run.isEmpty()){
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+      }
+      else{
+          return run.get();
+      }
+  }
+
+  // Post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/add")
+    void create(@RequestBody  Run run){
+      runRepository.create(run);
     }
+
+
+    // Update
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/update/{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id){
+            runRepository.update(run, id);
+    }
+
+
+
+    // Delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/delete/{id}")
+    void delete(@PathVariable Integer id){
+        runRepository.delete(id);
+    }
+
 }
--- src/main/java/dev/pdeep1506/runners/run/RunRepository.java
+++ src/main/java/dev/pdeep1506/runners/run/RunRepository.java
@@ -1,9 +1,64 @@
 package dev.pdeep1506.runners.run;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+
+@Repository
 public class RunRepository {
 
     private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll(){
+        return runs;
+    }
+
+    Optional<Run> FindById(Integer Id){
+        return  runs.stream()
+                .filter(runs -> runs.id() == Id)
+                .findFirst();
+
+    }
+
+    void create(Run run){
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id){
+            Optional<Run> existingRun = FindById(id);
+
+            if(existingRun.isPresent()){
+                runs.set(runs.indexOf(existingRun.get()),run);
+            }
+    }
+
+    void delete(Integer id){
+        runs.removeIf(run -> run.id().equals(id));
+    }
+
+    @PostConstruct
+    private  void  init(){
+        runs.add(new Run(1,
+                "Monday Morinin run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(20, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR
+                ));
+
+        runs.add(new Run(2,
+                "Monday Evening run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(40, ChronoUnit.MINUTES),
+                5,
+                Location.OUTDOOR
+        ));
+    }
+
 }
